dff6dfbdb718ca326e31320f7bbbf58a7d43ad4a
Esat Özkan <68530720+esatozkan@users.noreply.github.com>
2023-10-07 11:56:47 +0300

    yüklenmeye hazır hale getirildi

--- android/app/src/main/kotlin/com/example/zooventure/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.zooventure
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/models/animal_model.dart
+++
@@ -1,8 +0,0 @@
-class Animal {
-  late String name;
-  late String voice;
-  late String image;
-  late String realImage;
-  late bool isVisible;
-  late bool isCorrectAnswer;
-}
