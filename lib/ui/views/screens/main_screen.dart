73ff3d5fdc1e87374bf26946b259531100d5c6bc
Esat Özkan <68530720+esatozkan@users.noreply.github.com>
2024-01-18 17:43:52 +0300

    main screen safearea kaldırıldı

--- lib/ui/views/screens/main_screen.dart
+++ lib/ui/views/screens/main_screen.dart
@@ -61,96 +61,87 @@ class _MainScreenState extends State<MainScreen> {
     final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: SafeArea(
-        child: Consumer<PageChangedProvider>(
-          builder: (context, pageChangedProvider, _) => pageIndex
-                  .contains(pageChangedProvider.getPageChanged)
-              ? Stack(
-                  children: [
-                    Stack(
-                      children: [
-                        Image.asset(
-                          "assets/bottom_navbar_icon/screensBackground.png",
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height,
-                          fit: BoxFit.cover,
-                        ),
-                        Center(
-                          child: Image.asset(
-                            pageChangedProvider.getPageChanged == 0
-                                ? "assets/bottom_navbar_icon/animalsIcon.png"
-                                : pageChangedProvider.getPageChanged == 1
-                                    ? "assets/bottom_navbar_icon/animalVoiceIcon.png"
-                                    : "assets/bottom_navbar_icon/gameScreenIcon.png",
-                            color: Colors.black12,
-                            height: MediaQuery.of(context).size.height*2/3,
-                            width: MediaQuery.of(context).size.height*2/3,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const OnBoardingControlWidget(),
-                    const TitleWidget(),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        left: size.width < 1100 ? 100 : 150,
-                        right: size.width < 1100 ? 100 : 150,
-                        top: size.width < 1100 ? 100 : 150,
-                        bottom: (googleAdsProvider.bannerAd != null &&
-                                !inAppPurchaseProvider.getIsPremiumSubscribed &&
-                                !inAppPurchaseProvider.getRemoveAdIsSubscribed)
-                            ? googleAdsProvider.bannerAd!.size.height.toDouble()
-                            : 0,
-                      ),
-                      child: PageView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: pages.length,
-                        controller: pageController,
-                        onPageChanged: (index) {
-                          pageChangedProvider.pageChangedFunction(
-                              pageChangedProvider.getPageChanged);
-                        },
-                        itemBuilder: (context, index) =>
-                            pages[pageChangedProvider.getPageChanged],
-                      ),
-                    ),
-                    Visibility(
-                      visible: (googleAdsProvider.bannerAd != null &&
-                              !inAppPurchaseProvider.getIsPremiumSubscribed &&
-                              !inAppPurchaseProvider.getRemoveAdIsSubscribed)
-                          ? true
-                          : false,
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: SizedBox(
-                          width: googleAdsProvider.bannerAd != null
-                              ? googleAdsProvider.bannerAd!.size.width
-                                  .toDouble()
-                              : 0,
-                          height: googleAdsProvider.bannerAd != null
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image:
+                AssetImage("assets/bottom_navbar_icon/screensBackground.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Consumer<PageChangedProvider>(
+            builder: (context, pageChangedProvider, _) => pageIndex
+                    .contains(pageChangedProvider.getPageChanged)
+                ? Stack(
+                    children: [
+                      const OnBoardingControlWidget(),
+                      const TitleWidget(),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          left: size.width < 1100 ? 100 : 150,
+                          right: size.width < 1100 ? 100 : 150,
+                          top: size.width < 1100 ? 100 : 150,
+                          bottom: (googleAdsProvider.bannerAd != null &&
+                                  !inAppPurchaseProvider
+                                      .getIsPremiumSubscribed &&
+                                  !inAppPurchaseProvider
+                                      .getRemoveAdIsSubscribed)
                               ? googleAdsProvider.bannerAd!.size.height
                                   .toDouble()
                               : 0,
-                          child: googleAdsProvider.bannerAd != null
-                              ? AdWidget(ad: googleAdsProvider.bannerAd!)
-                              : const Text(""),
+                        ),
+                        child: PageView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: pages.length,
+                          controller: pageController,
+                          onPageChanged: (index) {
+                            pageChangedProvider.pageChangedFunction(
+                                pageChangedProvider.getPageChanged);
+                          },
+                          itemBuilder: (context, index) =>
+                              pages[pageChangedProvider.getPageChanged],
                         ),
                       ),
-                    )
-                  ],
-                )
-              : PageView.builder(
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: pages.length,
-                  controller: pageController,
-                  onPageChanged: (index) {
-                    pageChangedProvider.pageChangedFunction(
-                        pageChangedProvider.getPageChanged);
-                  },
-                  itemBuilder: (context, index) =>
-                      pages[pageChangedProvider.getPageChanged],
-                ),
+                      Visibility(
+                        visible: (googleAdsProvider.bannerAd != null &&
+                                !inAppPurchaseProvider.getIsPremiumSubscribed &&
+                                !inAppPurchaseProvider.getRemoveAdIsSubscribed)
+                            ? true
+                            : false,
+                        child: Align(
+                          alignment: Alignment.bottomCenter,
+                          child: SizedBox(
+                            width: googleAdsProvider.bannerAd != null
+                                ? googleAdsProvider.bannerAd!.size.width
+                                    .toDouble()
+                                : 0,
+                            height: googleAdsProvider.bannerAd != null
+                                ? googleAdsProvider.bannerAd!.size.height
+                                    .toDouble()
+                                : 0,
+                            child: googleAdsProvider.bannerAd != null
+                                ? AdWidget(ad: googleAdsProvider.bannerAd!)
+                                : const Text(""),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                : PageView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: pages.length,
+                    controller: pageController,
+                    onPageChanged: (index) {
+                      pageChangedProvider.pageChangedFunction(
+                          pageChangedProvider.getPageChanged);
+                    },
+                    itemBuilder: (context, index) =>
+                        pages[pageChangedProvider.getPageChanged],
+                  ),
+          ),
         ),
       ),
     );
